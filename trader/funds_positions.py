9862ce09dfbb87bb186250f531cc64153734a9d9
YILINQ <victlin@outlook.com>
2021-09-28 01:10:06 +0800

    html decode

--- trader/funds_positions.py
+++ trader/funds_positions.py
@@ -2,6 +2,7 @@ from urllib import request
 from bs4 import BeautifulSoup
 import re
 import pymysql
+import requests
 
 fundShareList = []
 
@@ -30,6 +31,10 @@ def handle(a):
         new = [a[0], a[1], a[4]]
         fundShareList.append(new)
 
+def get_data(url):
+    r = requests.get(url=url, headers=head)
+    print(r.text)
+
 
 def main():
     funds = []
@@ -39,10 +44,31 @@ def main():
     send = request.Request(url="http://fund.eastmoney.com/js/fundcode_search.js", headers=head)
     response = request.urlopen(send)
     js = response.read().decode('utf-8')
-    list1 = js[9:-2].split('],[')
+    list1 = js[11:-2].split('],[')
     for i in range(len(list1)):
         fund = str(list1[i]).replace('"', '')
-        print(fund)
+        fund = fund.split(',')
+        fund_ids.append(fund[0])
+        funds.append(fund)
+
+    for i in range(len(fund_ids)):
+        url = "http://fundf10.eastmoney.com/FundArchivesDatas.aspx?type=jjcc&code={}&topline=10&year=2021&month=&rt=0.21822537857648627" \
+            .format(fund_ids[i])
+        get_data(url)
+
+    # while fundNum < len(fund_ids):
+    #
+
+    #     send = request.Request(url, headers=head)
+    #     response = request.urlopen(send, timeout=1)
+    #     html = response.read().decode('utf-8')
+    #     bs = BeautifulSoup(html, 'html.parser')
+    #     find_list = bs.find_all("tbody")
+    #     if find_list != []:
+    #         tr = find_list[0].find_all("tr")
+    #         print(tr)
+    #     fundNum += 1
+
 
 if __name__ == "__main__":
     main()
